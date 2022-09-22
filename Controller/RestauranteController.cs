0aa0ce749accaea8a6ca25cb4f743421066215ab
thitoGloor <102191410+thitoGloor@users.noreply.github.com>
2022-09-22 00:51:35 -0300

    atualizacao controller

--- Controller/RestauranteController.cs
+++ Controller/RestauranteController.cs
@@ -1,28 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using Trabalho4SemestreApi.Data;
 using Trabalho4SemestreApi.Models;
 
 namespace Trabalho4SemestreApi.Controllers
 {
     [ApiController]
-    [Route("{Controller}")]
+    [Route("[Controller]")]
     public class RestauranteController : ControllerBase
     {
         private static List<Restaurante> restaurantes = new List<Restaurante>();
-       
+        private readonly Contexts _contexto;
+        public RestauranteController(Contexts contexto)
+        {
+            _contexto = contexto;
+        }
+
         [HttpPost]
         public void AddRestaurante([FromBody] Restaurante restaurante)
         {
-            restaurantes.Add(restaurante);
+            _contexto.Restaurante.Add(restaurante);
+            _contexto.SaveChanges();
         }
         [HttpGet]
         public IActionResult GetRestaurantes()
         {
-            return Ok(restaurantes);
+            return Ok(_contexto.Restaurante.ToList());
         }
-        [HttpGet("{Id}")]
-        public IActionResult GetRestaurantesById(int id)
+        [HttpGet("{id}")]
+        public ActionResult GetRestaurantesById(int id)
         {
-            Restaurante restaurante = restaurantes.FirstOrDefault (restaurante => restaurante.RestauranteId ==id);
+            Restaurante restaurante = _contexto.Restaurante.FirstOrDefault (restaurante => restaurante.RestauranteId ==id);
             if (restaurante != null)
             {
                 return Ok(restaurante);
@@ -30,6 +37,16 @@ namespace Trabalho4SemestreApi.Controllers
             else
             {
                 return NotFound();
+            }
+        }
+        [HttpDelete("{id}")]
+        public void DeletarRestaurante(int id)
+        {
+            var restaurante = _contexto.Restaurante.FirstOrDefault(restaurante => restaurante.RestauranteId == id);
+            if(restaurante != null)
+            {
+                _contexto.Restaurante.Remove(restaurante);
+                _contexto.SaveChanges();
             }
         }
     }
