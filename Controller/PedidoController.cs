025c2403a8c1fcdd3d3ae9980d3ba265e5f2db5d
thitoGloor <102191410+thitoGloor@users.noreply.github.com>
2022-09-22 00:50:03 -0300

    atualizacao controller

--- Controller/PedidoController.cs
+++ Controller/PedidoController.cs
@@ -1,29 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using Trabalho4SemestreApi.Data;
 using Trabalho4SemestreApi.Models;
 
 namespace Trabalho4SemestreApi.Controllers
 {
     [ApiController]
-    [Route("{Controller}")]
+    [Route("[Controller]")]
     public class PedidoController : ControllerBase
     {
+
         private static List<Pedido> pedidos = new List<Pedido>();
+        public readonly Contexts _contexto;
+        public PedidoController(Contexts contexto)
+        {
+            _contexto = contexto;
+        }
+
+
         [HttpPost]
         public void AddPedido([FromBody] Pedido pedido)
         {
-            pedidos.Add(pedido);
+            _contexto.Pedido.Add(pedido);
+            _contexto.SaveChanges();
 
         }
         [HttpGet]
         public IActionResult GetPedidos()
-           {
-            return Ok(pedidos);
-            }
-        [HttpGet ("{id}")]
-        public IActionResult GetPedidosById(int id)
         {
-            Pedido pedido = pedidos.FirstOrDefault(pedido =>pedido.Id == id);
-            if(pedido != null)
+            return Ok(_contexto.Pedido.ToList());
+        }
+        [HttpGet("{id}")]
+        public ActionResult GetPedidosById(int id)
+        {
+            Pedido pedido = _contexto.Pedido.FirstOrDefault(pedido => pedido.Id == id);
+            if (pedido != null)
             {
                 return Ok(pedido);
             }
@@ -32,8 +42,21 @@ namespace Trabalho4SemestreApi.Controllers
                 return NotFound();
             }
         }
+        [HttpDelete("{id}")]
+        public void DeletarPedido(int id)
+            {
+            
+            
+                var pedido = _contexto.Pedido.FirstOrDefault(pedido => pedido.Id == id);
+            if (pedido != null)
+            { 
+                _contexto.Pedido.Remove(pedido);
+                _contexto.SaveChanges();
+            }
+            }
         
 
 
     }
 }
+
